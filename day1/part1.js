90624c605ad83376af5ee8ba9a3e86f495bd5f0a
Sansarun Sukawongviwat <sansarun.sukawongviwat@agoda.com>
2022-12-01 13:01:46 +0700

    refactor

--- day1/part1.js
+++ day1/part1.js
@@ -3,9 +3,9 @@ const _ = require("lodash");
 const fs = require("fs");
 const input = fs.readFileSync("input.txt", "utf8");
 
-const elves_cal = input.split("\n\n").map((calsString) => {
-  return _.sum(calsString.split("\n").map((cal) => Number(cal)));
-});
+const elves_cal = input
+  .split("\n\n")
+  .map((calsString) => _.sum(calsString.split("\n").map((cal) => Number(cal))));
 
-top_cal = _.max(elves_cal)
+top_cal = _.max(elves_cal);
 console.log(top_cal);
--- day1/part2.js
+++ day1/part2.js
@@ -3,9 +3,9 @@ const _ = require("lodash");
 const fs = require("fs");
 const input = fs.readFileSync("input.txt", "utf8");
 
-const elves_cal = input.split("\n\n").map((calsString) => {
-  return _.sum(calsString.split("\n").map((cal) => Number(cal)));
-});
+const elves_cal = input
+  .split("\n\n")
+  .map((calsString) => _.sum(calsString.split("\n").map((cal) => Number(cal))));
 
 sum_top3_cal = _.sum(_.takeRight(elves_cal.sort(), 3));
-console.log(sum_top3_cal)
+console.log(sum_top3_cal);
